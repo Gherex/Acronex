9a52d2e344f22ae1882ed0a139cad7f9d798e171
Gherex <germilagger@hotmail.com>
2024-01-17 12:22:16 -0300

    Agregue las tablas faltantes en DetallesMaquina

--- src/components/DetallesMaquina.js
+++ src/components/DetallesMaquina.js
@@ -37,7 +37,7 @@ const DetallesMaquina = () => {
     <div className="contenedor-detalles-maquina">
       <div className="bloque-superior">
         <h1> {maquinaData.description} </h1>
-        <p> ({maquinaData.id}) </p>
+        <p> {maquinaData.id} </p>
       </div>
       <div className="bloque-izq">
         <div className="indicadores">
@@ -48,7 +48,7 @@ const DetallesMaquina = () => {
         </div>
         <div className='empresa'>Empresa <div className="nombre-empresa">{maquinaData.company}</div></div>
         <div className='clase'>Clase <div className="tipo-clase">{maquinaData.class}</div></div>
-        <div className='estado'> Estado 
+        <div className='estado'> Estado
           <div className="contenedor-estado">
             <div className={`circulo-estado ${maquinaData.working ? 'circulo-verde' : 'circulo-rojo'}`}></div>
             <div className="valor-estado">{maquinaData.working ? "En movimiento" : "Detenida"} </div>
@@ -57,7 +57,63 @@ const DetallesMaquina = () => {
         <div className='ult-actu'>Última actualización <div className="fecha-ult-actu">{modificarFecha(maquinaData.last_update)}</div></div>
       </div>
       <div className="bloque-der">
-        Aqui van 3 tablas
+        <div className="tabla">
+          <h2>General</h2>
+          <div className="fila">
+            <h3>Cosechand</h3>
+            <p> - </p> 
+          </div>
+          <div className="fila">
+            <h3>Batería interna</h3>
+            <p> {maquinaData.last[19]} V </p>
+          </div>
+          <div className="fila">
+            <h3>Batería vehículo</h3>
+            <p> {maquinaData.last[20]} V </p>
+          </div>
+          <div className="fila">
+            <h3>Uso Combustible</h3>
+            <p> {maquinaData.last[265]} l/hora </p>
+          </div>
+        </div>
+        <div className="tabla">
+          <h2>Clima</h2>
+          <div className="fila">
+            <h3>Temperatura</h3>
+            <p>{maquinaData.last[200]} °C </p> 
+          </div>
+          <div className="fila">
+            <h3>Humedad</h3>
+            <p> {maquinaData.last[201]} % </p>
+          </div>
+          <div className="fila">
+            <h3>Dirección de viento</h3>
+            <p> SE({maquinaData.last[203]}°) </p>
+          </div>
+          <div className="fila">
+            <h3>Velocidad de viento</h3>
+            <p> {maquinaData.last[204]} km/h </p>
+          </div>
+        </div>
+        <div className="tabla">
+          <h2>Operación</h2>
+          <div className="fila">
+            <h3>Velocidad</h3>
+            <p> {maquinaData.last[25]} km/h </p> 
+          </div>
+          <div className="fila">
+            <h3>Presión</h3>
+            <p> {maquinaData.last[202]} bar </p>
+          </div>
+          <div className="fila">
+            <h3>Producto / hectárea</h3>
+            <p> {maquinaData.last[281]} litros/Ha </p>
+          </div>
+          <div className="fila">
+            <h3>Ancho</h3>
+            <p> {maquinaData.last[393]} m </p>
+          </div>
+        </div>
       </div>
     </div>
   );
